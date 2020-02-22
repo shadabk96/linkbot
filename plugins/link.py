56598883a675f738248726b7547d46c0b88a2cf4
Shadab Khan <shadabk96@gmail.com>
2020-02-22 16:08:59 +0530

    init

---
+++ plugins/link.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from mmpy_bot.bot import listen_to
+from mmpy_bot.bot import respond_to
+
+@listen_to('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+def link_listen(message):
+    # get relevant info from message 
+    author = message._get_sender_name()
+    channel = message.get_channel_name()
+    message_text = message.get_message()
+    print "author " + author
+    print "channel " + channel
+    print "message_text " + message_text
+    print message
+    message.reply('link reached')
+
+    # extract link from message
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message_text)
+    print urls
+
+    # store in db
+
+@listen_to('test')
+def test_listen(message):
+    message.reply("test reached 1")
+    message.reply("test reached 2")
