2cec4c68c40c832ee1d16abff13f3671bd2e53ac
Shadab Khan <shadabk96@gmail.com>
2020-02-29 13:05:20 +0530

    Change db file location

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ load_dotenv(dotenv_path=__DIR__ + '/.env')
 if os.getenv('COFFEEBOT_DATABASE_URI') is not None:
     DATABASE_URI = os.getenv('COFFEEBOT_DATABASE_URI')
 else:
-    DATABASE_URI = 'sqlite:////' + __DIR__ + os.getenv('COFFEEBOT_DATABASE_FILENAME')
+    DATABASE_URI = 'sqlite:////' + __DIR__ + '/' + os.getenv('COFFEEBOT_DATABASE_FILENAME')
